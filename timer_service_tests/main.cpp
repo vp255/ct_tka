f44584f0d67c4a8d0be2694b88a718bd0fb0a19b
Vincent Phan <vphan920@gmail.com>
2021-02-22 15:32:53 -0500

    pruned for submission

--- timer_service_tests/main.cpp
+++ timer_service_tests/main.cpp
@@ -2,8 +2,6 @@
 #include "timer_service_tests/DemoTimerServiceClient.h"
 #include "timer_service_tests/PerformanceTests.h"
 #include "timer_service/TimerService.h"
-#include "timer_service/TimerService_map_impl.h"
-#include "timer_service/TimerService_priority_queue_impl.h"
 
 int main() {
   RunDemoListenerTests();
@@ -12,26 +10,9 @@ int main() {
   timer_service_tests::RunCorrectnessTests<TimerService>();
   std::cout << "Passed Correctness Tests for TimerService" << std::endl;
 
-  //std::cout << "Running CorrectnessTests for TimerService_map_impl" << std::endl;
-  //timer_service_tests::RunCorrectnessTests<TimerService_map_impl>();
-  //std::cout << "Passed Correctness Tests for TimerService_map_impl" << std::endl;
-
-  //std::cout << "Running CorrectnessTests for TimerService_priority_queue_impl" << std::endl;
-  //timer_service_tests::RunCorrectnessTests<TimerService_priority_queue_impl>();
-  //std::cout << "Passed Correctness Tests for TimerService_priority_queue_impl" << std::endl;
-
-
   std::cout << "Running PerformanceTests for TimerService\n";
   timer_service_tests::RunPerformanceTests<TimerService>();
   std::cout << "\n\n";
-
-  //std::cout << "Running PerformanceTests for TimerService_map_impl\n";
-  //timer_service_tests::RunPerformanceTests<TimerService_map_impl>();
-  //std::cout << "\n\n";
-
-  //std::cout << "Running PerformanceTests for TimerService_priority_queue_impl\n";
-  //timer_service_tests::RunPerformanceTests<TimerService_priority_queue_impl>();
-  //std::cout << "\n\n";
 
   return 0;
 }
